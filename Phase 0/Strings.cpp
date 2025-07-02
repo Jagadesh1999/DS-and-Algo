e0f970adf99b9021f1dd05f6590410334b4b71eb
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-07-02 11:16:10 +0530

    Update Strings.cpp

--- Phase 0/Strings.cpp
+++ Phase 0/Strings.cpp
@@ -170,3 +170,41 @@ int main() {
     solve();
     return 0;
 }
+
+// 6. IO Problem 
+void solve() {
+    int x;
+    cin>>x;
+
+    string m;
+    cin>>m;
+
+    string y;
+    string z;
+
+    int count=0;
+    for(int i=0; m[i]!='.'; i++) {
+        y.push_back(m[i]);
+        count++;
+    }
+
+    for(int i=count+1; m[i]!='\0'; i++) {
+        z.push_back(m[i]);
+    }
+
+    int f = stoi(z)+x;
+    int l = stoi(y)+x;
+
+    cout << f << "." << l << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t; cin>>t;
+    while(t--) solve();
+    return 0;  
+}
+
+
